be9d8b7f6aac58f5291cd8344bd0e57350a4eded
nisarul-media <65971192+nisarul-media@users.noreply.github.com>
2021-01-10 03:45:25 +0600

    updates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+const isChecked = document.getElementById("toggle").checked;
+console.log(isChecked);
+
+if (isChecked == true) {
+  // Changing The CSS
+  const style = document.createElement("style");
+  style.innerHTML = `
+    body {
+      background-color: #1E1E1E;
+    }
+    :root {
+      --color: #97A1EF;
+      --color-dark: #394bed;
+      --font-size: 1em;
+      --title-color: #ffffff;
+      --font-color: #a2a2a2;
+    }
+    header .hero-area h1 span {
+      background-image: linear-gradient(227deg, #5daffc, #9e74ff, #ff65ff);
+    }
+    `;
+  document.head.appendChild(style);
+}
+
 function check() {
   const isChecked = document.getElementById("toggle").checked;
   console.log(isChecked);
@@ -16,6 +40,9 @@ function check() {
       --title-color: #ffffff;
       --font-color: #a2a2a2;
     }
+    header .hero-area h1 span {
+      background-image: linear-gradient(227deg, #5daffc, #9e74ff, #ff65ff);
+    }
     `;
     document.head.appendChild(style);
 
@@ -33,6 +60,9 @@ function check() {
       --font-size: 1em;
       --title-color: #141c3a;
       --font-color: #222222;
+    }
+    header .hero-area h1 span {
+      background-image: linear-gradient(227deg, #3083d0, #6030d0, #330867);
     }
     `;
     document.head.appendChild(style);
